a705e13fe6d30a97d1c746b3a906372b70a2f11b
mreleftheros <eleftheros_all@protonmail.com>
2023-01-31 20:39:59 +0200

    Add json stringify

--- functions/api/auth/login.js
+++ functions/api/auth/login.js
@@ -1,12 +1,12 @@
 export const onRequestPost = ctx => {
   return new Response(
-    {
+    JSON.stringify({
       data: {
         id: 0,
         username: "test",
         email: "test@gmail.com",
       },
-    },
+    }),
     {
       status: 200,
       headers: {
