455a1e6fc62acfbf21744c37ac3ffafb6632c0ae
mreleftheros <eleftheros_all@protonmail.com>
2023-01-31 19:57:21 +0200

    Update lib urls to point to functions

--- solid/src/lib/auth.js
+++ solid/src/lib/auth.js
@@ -1,7 +1,8 @@
-const BASE_URL = "http://localhost:5000/api/auth";
+const BASE_URL_FUNCTIONS = window.location.origin + "/api/auth";
+const BASE_URL_EXPRESS = "http://localhost:5000/api/auth";
 
 export const signup_post = async (username, email, password) => {
-  const res = await fetch(BASE_URL + "/signup", {
+  const res = await fetch(BASE_URL_FUNCTIONS + "/signup", {
     method: "POST",
     credentials: "include",
     headers: {
@@ -14,7 +15,7 @@ export const signup_post = async (username, email, password) => {
 };
 
 export const login_post = async (email, password) => {
-  const res = await fetch(BASE_URL + "/login", {
+  const res = await fetch(BASE_URL_FUNCTIONS + "/login", {
     method: "POST",
     credentials: "include",
     headers: {
@@ -27,7 +28,7 @@ export const login_post = async (email, password) => {
 };
 
 export const me_get = async () => {
-  const res = await fetch(BASE_URL + "/me", {
+  const res = await fetch(BASE_URL_FUNCTIONS + "/me", {
     credentials: "include",
   });
   const { error, errors, data } = await res.json();
@@ -35,7 +36,7 @@ export const me_get = async () => {
 };
 
 export const logout_get = async () => {
-  const res = await fetch(BASE_URL + "/logout", {
+  const res = await fetch(BASE_URL_FUNCTIONS + "/logout", {
     credentials: "include",
   });
   const { error, errors, data } = await res.json();
